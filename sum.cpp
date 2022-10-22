f74296fb1472a073b5a6ce2e0e6d87a54e81d12e
KristianPetani <116462144+KristianPetani@users.noreply.github.com>
2022-10-22 22:39:00 +0200

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,10 @@
+// Scrivere un programma che legge due interi e ne stampa la somma.
+
+#include <iostream>
+using namespace std;
+int main () {
+  int a, b;
+  cout << "Inserire due numeri interi: \n";
+  cin >> a >> b;
+  cout << "La somma vale " << a+b << endl;
+}
