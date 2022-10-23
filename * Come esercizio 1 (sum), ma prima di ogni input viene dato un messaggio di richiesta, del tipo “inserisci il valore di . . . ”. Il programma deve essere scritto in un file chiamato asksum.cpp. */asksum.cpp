5949820b4b52081defca73235747ec3da6d14e0b
KristianPetani <116462144+KristianPetani@users.noreply.github.com>
2022-10-23 15:35:50 +0200

    asksum.cpp

---
+++ * Come esercizio 1 (sum), ma prima di ogni input viene dato un messaggio di richiesta, del tipo “inserisci il valore di . . . ”. Il programma deve essere scritto in un file chiamato asksum.cpp. */asksum.cpp
@@ -0,0 +1,13 @@
+/* Come esercizio 1 (sum), ma prima di ogni input viene dato un messaggio di richiesta, del tipo “inserisci il valore di . . . ”. Il
+programma deve essere scritto in un file chiamato asksum.cpp. */
+#include <iostream>
+using namespace std;
+int main() {
+int a, b;
+int sum = a+b;
+cout << "Inserisci il valore di a: \n";
+cin >> a;
+cout << "Inserisci il valore di b: \n";
+cin >> b;
+cout << "La somma vale: " << sum << endl;
+}
